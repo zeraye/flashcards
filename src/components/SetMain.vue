f11ebbd8fc0a2cab7f3cc78324ab91d660d83cef
zeraye <zeraye@protonmail.com>
2023-08-06 15:25:48 +0200

    feat: disable buttons after all cards learned

--- src/components/SetMain.vue
+++ src/components/SetMain.vue
@@ -17,10 +17,10 @@ const props = defineProps([
   <div>description: {{ set.description }}</div>
   <div @click="flipCard">card: {{ cardText }}</div>
   <div>progress: {{ cardIndex + 1 }}/{{ set.flashcards.length }}</div>
-  <button @click="nextCard">incorrectly</button>
-  <button @click="nextCard">correctly</button>
+  <button @click="nextCard" :disabled="cardIndex + 1 === set.flashcards.length">incorrectly</button>
+  <button @click="nextCard" :disabled="cardIndex + 1 === set.flashcards.length">correctly</button>
   <br />
-  <button @click="prevCard" :disabled="cardIndex <= 0">undo</button>
+  <button @click="prevCard" :disabled="cardIndex === 0">undo</button>
   <button @click="shuffleCards">shuffle (currently {{ isShuffled ? 'on' : 'off' }})</button>
   <br />
   <button @click="setSceneState('flashcards')">flashcards</button>
