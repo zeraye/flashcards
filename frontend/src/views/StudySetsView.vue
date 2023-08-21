1b3f1b32f806555177a748321cb507a643882a46
zeraye <zeraye@protonmail.com>
2023-08-21 17:05:58 +0200

    feat: list user sets at `/sets`

--- frontend/src/views/StudySetsView.vue
+++ frontend/src/views/StudySetsView.vue
@@ -1,3 +1,32 @@
+<script setup lang="ts">
+import { ref, onMounted } from 'vue'
+
+import type { Ref } from 'vue'
+
+import type { SetType } from '@/types/SetType'
+
+import { useUrlStore } from '@/stores/url'
+import { useUserStore } from '@/stores/user'
+
+const urlStore = useUrlStore()
+const userStore = useUserStore()
+
+const sets: Ref<Array<SetType> | null> = ref(null)
+const username = ref('')
+
+onMounted(async () => {
+  const response = await fetch(`${urlStore.BACKEND_API_URL}/users/${userStore.username}`)
+  const data = await response.json()
+  sets.value = data.sets
+  username.value = data.username
+})
+</script>
+
 <template>
-  <main>Sets</main>
+  <main>{{ username }}'s sets</main>
+  <div v-if="sets !== null">
+    <li v-for="set in sets">
+      <RouterLink :to="`/sets/${set.id}`">{{ set.title }}</RouterLink>
+    </li>
+  </div>
 </template>
